227b341cb200a909416d9a31ac907b1faac445f5
Loyalchandu <161041922+Loyalchandu@users.noreply.github.com>
2024-06-20 18:41:31 +0530

    Add files via upload

---
+++ compound_intrest.py
@@ -0,0 +1,5 @@
+principle = int(input("enter the principle:"))
+rate = int(input("enter the rate:"))
+time = int(input("enter the time:"))
+ci = (principle*rate*time)/100
+print("compound intrest = ",ci)
