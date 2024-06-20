2cbfddd25b0541b0dcdc7d71365d3cedbd39338c
Loyalchandu <161041922+Loyalchandu@users.noreply.github.com>
2024-06-20 18:50:50 +0530

    Add files via upload

---
+++ greatest_of_3.py
@@ -0,0 +1,10 @@
+x= int(input())
+y=int(input())
+z=int(input())
+
+if (x>y) and (x>z):
+    print(x,"is greater")
+elif (y>x) and (y>z):
+    print(y,"is greater")
+elif (z>x) and (z>y):
+    print(z,"is greater")
