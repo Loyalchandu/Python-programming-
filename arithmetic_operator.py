8715c00e724ff4a9c48f0af25d776957b70a1059
Loyalchandu <161041922+Loyalchandu@users.noreply.github.com>
2024-06-20 18:40:58 +0530

    Add files via upload

---
+++ arithmetic_operator.py
@@ -0,0 +1,13 @@
+a = input("enter the value of A: ")
+b = input("enter the value of B: ")
+# Arithmetic operations
+print("addition:", int(a) + int(b))
+print("multiplication:", int(a) * int(b))
+print("subtraction:", int(a) - int(b))
+print("division:", int(a) / int(b))
+print("moduls:", int(a) % int(b))
+print("quotient:", int(a) // int(b))
+print("exponent:", int(a) ** int(b))
+num_1 = 20
+num_2 = 30
+print(num_1+num_2)
