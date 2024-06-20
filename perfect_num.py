9573b9377945136f5c99fc7939bffa760b660def
Loyalchandu <161041922+Loyalchandu@users.noreply.github.com>
2024-06-20 19:10:50 +0530

    Add files via upload

---
+++ perfect_num.py
@@ -0,0 +1,9 @@
+num=int(input())
+Sum = 0
+for i in range(1, num):
+    if(num % i == 0):
+        Sum = Sum + i
+if (Sum == num):
+    print("Number is a Perfect Number.")
+else:
+    print("Number is not a Perfect Number.")
